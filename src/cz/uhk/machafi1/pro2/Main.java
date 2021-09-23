bb6a9961c5ed256ad0512daadc862165009b47a9
Filip Macháček <filip@pallamedia.cz>
2021-09-23 13:34:29 +0200

    Hello message

--- src/cz/uhk/machafi1/pro2/Main.java
+++ src/cz/uhk/machafi1/pro2/Main.java
@@ -3,6 +3,6 @@ package cz.uhk.machafi1.pro2;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Test");
+        System.out.println("Hello");
     }
 }
